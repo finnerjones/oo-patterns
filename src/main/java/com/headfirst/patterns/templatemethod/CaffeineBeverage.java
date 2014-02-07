ee45b9216411c7ac85d937ac551871c774801230
finnerjones <finnseamus@gmail.com>
2014-02-07 22:52:38 +0100

    implemented CaffeineBeverage

--- src/main/java/com/headfirst/patterns/templatemethod/CaffeineBeverage.java
+++ src/main/java/com/headfirst/patterns/templatemethod/CaffeineBeverage.java
@@ -1,5 +1,24 @@
 package com.headfirst.patterns.templatemethod;
 
 public abstract class CaffeineBeverage {
+	public void prepareRecipe() {
+		boilWater();
+		brew();
+		pourInCup();
+		addCondiments();
+	}
 
+	private void boilWater() {
+		System.out.println("boiling water");
+		
+	}
+
+	private void pourInCup() {
+		System.out.println("pouring in cup");
+		
+	}
+	
+	public abstract void brew();
+	
+	public abstract void addCondiments();
 }
